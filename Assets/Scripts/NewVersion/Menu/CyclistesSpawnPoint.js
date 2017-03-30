412f091c5210f5b35f8571e2d7b543e46d097388
Yann3d <yannick.jouneau3d@free.fr>
2017-03-30 20:19:45 +0200

    Menu WIP

---
+++ Assets/Scripts/NewVersion/Menu/CyclistesSpawnPoint.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var cyclistes : GameObject[];
+var spawnGap : Vector2;
+
+function Start () {
+	Spawn();
+}
+
+function Update () {
+	
+}
+
+function Spawn (){
+	if (cyclistes.Length > 0){
+		var cyclIdx : int = Random.Range(0, cyclistes.Length);
+		Instantiate(cyclistes[cyclIdx], transform.position, Quaternion.identity);
+		Wait();
+	}
+}
+
+function Wait(){
+	var waitTime : float = Random.Range(spawnGap.x, spawnGap.y);
+	yield WaitForSeconds(waitTime);
+	Spawn();
+}
---
+++ Assets/Scripts/NewVersion/Menu/UIMenuManager.js
@@ -0,0 +1,103 @@
+﻿#pragma strict
+
+import UnityEngine.UI;
+import UnityEngine.EventSystems;
+
+class UIMenuManager extends MonoBehaviour {
+
+	var nameInput : GameObject;
+
+	var mainMenu : GameObject;
+	var difficultyMenu : GameObject;
+	var albumMenu : GameObject;
+	var succesMenu : GameObject;
+	var creditsScreen : GameObject;
+	var photoBigPicture : GameObject;
+	var photoBigPictureImage : Image;
+	var photoText : Text;
+	var screenCollider : GameObject;
+	private var currentMenu : GameObject;
+
+	private static var instance : UIMenuManager;
+	public static function Instance () : UIMenuManager {
+		return instance;
+	}
+
+	function Awake (){
+		if (instance != null){
+			Destroy (gameObject);
+		}
+		else {
+			instance = this;
+		}
+	}
+
+	function Start () {
+		currentMenu = mainMenu;
+		PlayCurrentMenuEnter();
+		currentMenu.SetActive(true);
+	}
+
+	function Update () {
+		
+	}
+
+	function OnClickPlay (){
+		PlayCurrentMenuLeave();
+		currentMenu = difficultyMenu;
+		PlayCurrentMenuEnter();
+		currentMenu.SetActive(true);
+	}
+
+	function OnClickAlbum (){
+		PlayCurrentMenuLeave();
+		currentMenu = albumMenu;
+		PlayCurrentMenuEnter();
+		currentMenu.SetActive(true);
+	}
+
+	function OnClickSucces (){
+		PlayCurrentMenuLeave();
+		currentMenu = succesMenu;
+		PlayCurrentMenuEnter();
+		currentMenu.SetActive(true);
+	}
+
+	function OnClickCredits (){
+		PlayCurrentMenuLeave();
+		currentMenu = creditsScreen;
+		PlayCurrentMenuEnter();
+		currentMenu.SetActive(true);
+	}
+
+	function OnClickQuitter (){
+		Application.Quit();
+	}
+
+	function OnClickBack (){
+		PlayCurrentMenuLeave();
+		currentMenu = mainMenu;
+		PlayCurrentMenuEnter();
+		currentMenu.SetActive(true);
+	}
+
+	function PlayCurrentMenuEnter (){
+		currentMenu.GetComponent.<Animation>().Play("AnyMenuAnimEnter");
+	}
+
+	function PlayCurrentMenuLeave (){
+		currentMenu.GetComponent.<Animation>().Play("AnyMenuAnimLeave");
+	}
+
+	function OnClickMiniature (){
+		photoBigPictureImage.sprite = EventSystem.current.currentSelectedGameObject.GetComponent.<Image>().sprite;
+		photoText.text = EventSystem.current.currentSelectedGameObject.name;
+		photoBigPicture.SetActive(true);
+		screenCollider.SetActive(true);
+	}
+
+	function OnClickScreenCollider (){
+		photoBigPicture.SetActive(false);
+		screenCollider.SetActive(false);
+	}
+}
