278d15496c0c4c8deaf41e9860055814e0c53a2d
Yann3D <yannick.jouneau3d@free.fr>
2017-04-01 00:27:16 +0200

    Success WIP

---
+++ Assets/Scripts/NewVersion/ScriptableObjects/Success/StepObject.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+import UnityEngine.UI;
+
+@script CreateAssetMenu (fileName ="StepObject_", menuName = "New Step Object")
+
+class StepObject extends ScriptableObject {
+	
+	@Header ("Step Object")
+	var objectName : String;
+	var objectPrefab : GameObject;
+}
---
+++ Assets/Scripts/NewVersion/ScriptableObjects/Success/SuccessObject.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+import UnityEngine.UI;
+
+@script CreateAssetMenu (fileName ="Success_", menuName = "New Success")
+
+class SuccessObject extends ScriptableObject {
+
+	@Header ("Success")
+	var successName : String;
+	var missionDescription : String;
+	var successSteps : SuccessStep[];
+	var running : boolean = false;
+	var ended : boolean = false;
+}
